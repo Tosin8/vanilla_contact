3e19b873a9487304924484acacf1875470e5910e
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-19 15:41:56 +0100

    Update new_contact.dart

--- lib/widgets/new_contact.dart
+++ lib/widgets/new_contact.dart
@@ -1,1 +1,13 @@
+class NewContactView extends StatefulWidget {
+  const NewContactView({super.key});
 
+  @override
+  State<NewContactView> createState() => _NewContactViewState();
+}
+
+class _NewContactViewState extends State<NewContactView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
