c44eb0113a850b7f25536ad5267bb1147a92b164
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-19 15:34:38 +0100

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,26 +13,28 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final contactBook = ContactBook();
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Contacts',
-            style: TextStyle(
-              color: Colors.white,
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'Contacts',
+              style: TextStyle(
+                color: Colors.white,
+              ),
             ),
+            backgroundColor: Colors.blue,
           ),
-          backgroundColor: Colors.blue,
-        ),
-        body: SafeArea(
-            child: ListView.builder(
-                itemCount: contactBook.length,
-                itemBuilder: (context, index) {
-                  final contact = contactBook.contact(atIndex: index)!;
-                  return ListTile(
-                    title: Text(contact.name),
-                  );
-                }),
-            // adding a new contact using a widget.
-            floatingActionButton: FloatingActionButton(onPressed: onPressed)));
+          body: ListView.builder(
+            itemCount: contactBook.length,
+            itemBuilder: (context, index) {
+              final contact = contactBook.contact(atIndex: index)!;
+              return ListTile(
+                title: Text(contact.name),
+              );
+            },
+          ),
+          // adding a new contact using a widget.
+          floatingActionButton: FloatingActionButton(onPressed: null)),
+    );
   }
 }
