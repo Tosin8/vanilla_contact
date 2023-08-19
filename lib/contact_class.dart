1c5d913d34f4254b295d0368868d765428ec24a6
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-19 14:37:14 +0100

    Update contact_class.dart

--- lib/contact_class.dart
+++ lib/contact_class.dart
@@ -9,4 +9,7 @@ class Contact {
 class ContactBook {
   ContactBook._sharedInstance();
   static final ContactBook _shared = ContactBook._sharedInstance();
+  factory ContactBook() => _shared;
+
+  final List<Contact> _contacts = []; // an empty list array to begin with.
 }
