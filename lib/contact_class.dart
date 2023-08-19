e4eebb4e94e33ff2d5aca37133c4363ce2ae33fe
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-19 14:35:31 +0100

    Update contact_class.dart

--- lib/contact_class.dart
+++ lib/contact_class.dart
@@ -1,5 +1,12 @@
 class Contact {
   final String name;
 
-  const Contact(this.name);
+  const Contact({
+    required this.name,
+  });
+}
+
+class ContactBook {
+  ContactBook._sharedInstance();
+  static final ContactBook _shared = ContactBook._sharedInstance();
 }
